2e99d5484333310d7bc83a714b488cd0091deb22
ziebamikolaj <ziebamikolaj03@gmail.com>
2024-05-14 00:41:08 +0200

    feat: :sparkles: language icons and repo's topics

--- src/components/RepoCard.tsx
+++ src/components/RepoCard.tsx
@@ -4,13 +4,15 @@ import { CiCalendar } from "react-icons/ci";
 import { FiGithub } from "react-icons/fi";
 import { PiLinkLight } from "react-icons/pi";
 
+import { LanguageIcon } from "./LanguageIcon";
+import { Badge } from "./ui/badge";
 import { TypographyH3, TypographyP } from "./ui/typographies";
 
 export const RepoCard = ({ repository }: { repository: Repository }) => {
   return (
     <div className="flex w-[20rem] flex-wrap justify-end rounded-xl bg-card md:w-[40rem] xl:w-[32rem] 2xl:w-[28rem]">
-      <div className="bg-card-secondary grid h-60 w-full place-items-center rounded-t-xl">
-        <FiGithub className="h-24 w-full" />
+      <div className="grid h-60 w-full place-items-center rounded-t-xl bg-card-secondary">
+        <FiGithub className="h-24 w-full text-card-foreground" />
       </div>
 
       <div className="w-full p-6">
@@ -33,11 +35,20 @@ export const RepoCard = ({ repository }: { repository: Repository }) => {
             {repository.description}
           </TypographyP>
         </div>
-
-        <TypographyP className="flex w-full content-end items-center justify-end gap-2">
-          <CiCalendar />
-          Updated {format(repository.updated_at, "MMMM yyyy")}
-        </TypographyP>
+        <div className="flex flex-wrap gap-2">
+          {repository.topics.map((topic) => (
+            <Badge variant="outline" key={topic}>
+              {topic}
+            </Badge>
+          ))}
+        </div>
+        <div className="flex items-end">
+          <LanguageIcon language={repository.language} />
+          <TypographyP className="flex w-full content-end items-center justify-end gap-2">
+            <CiCalendar />
+            Updated {format(repository.updated_at, "MMMM yyyy")}
+          </TypographyP>
+        </div>
       </div>
     </div>
   );
